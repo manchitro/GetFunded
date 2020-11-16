837434dd1883b43d0595f7a892ad3429403b7551
Asif Nawaz <asifnawaz5926@gmail.com>
2020-11-16 23:17:14 +0600

    Created user and db model

---
+++ models/UserModel.js
@@ -0,0 +1,49 @@
+const db = require('./db');
+
+module.exports ={
+
+	validate: function(user, callback){
+		
+		
+	},
+	getById: function(id, callback){
+		var sql = "select * from users WHERE id="+id;
+
+        db.getResults(sql, function(results){
+            callback(results);
+        });
+
+	},
+	getAll: function(callback){
+		var sql = "select * from users";
+		db.getResults(sql, function(results){
+			callback(results);
+		});
+
+	},
+	insert: function(user, callback){
+		var sql = "insert into users (name, username, email, password, usertype, createdat) VALUES ('"+user.name+"' , '"+user.username+"' ,  '"+user.email+"' , '"+user.password+"' , '"+user.usertype+"', current_timestamp() )";
+		
+		//console.log(sql);
+
+		db.execute(sql, function(status){
+			callback(status);
+		});
+	},
+	update: function(user, callback){
+		//var sql = "update user set ('', '"+user.username+"' , '"+user.password+"' , '"+user.type+"')";
+		var sql= "UPDATE users SET name='"+user.name+"', username ='"+user.username +"', email='"+user.email +"' where id="+user.id;
+		//console.log(sql);
+        console.log(sql);
+		db.execute(sql, function(status){
+			callback(status);
+		});
+	},
+	delete: function(id, callback){
+		var sql = "DELETE FROM users WHERE id="+id;
+        db.execute(sql, function(status){
+            callback(status);
+        });
+
+	}
+}
