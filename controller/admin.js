bdb86d547492c7469c829a45a788e99405ec4b80
Sazid <initialsaremsu@gmail.com>
2020-11-19 21:31:46 +0600

    new ui for login and signup, admin and userSupport page and controller

---
+++ controller/admin.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const userModel = require.main.require("./models/userModel");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("admin/index");
+});
+
+router.get("/login", (req, res) => {
+  res.render("admin/login");
+});
+
+router.post("/login", (req, res) => {
+    var user = {
+		username: req.body.username,
+        password: req.body.password,
+        userType: "admin"
+	};
+
+	userModel.validate(user, function(status){
+		if(status){
+			res.cookie('uname', req.body.username);
+			res.redirect('/home');	
+		}else{
+			res.redirect('/login');
+		}
+	});
+});
+
+module.exports = router;
