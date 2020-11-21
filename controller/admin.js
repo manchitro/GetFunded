78ce65ba401462e9333b3be500cf46087d1575cf
Sazid <initialsaremsu@gmail.com>
2020-11-21 22:42:06 +0600

    Created User support managemnet (view, add, edit, delete, search)

--- controller/admin.js
+++ controller/admin.js
@@ -137,7 +137,7 @@ router.post("/moderators/edit", (req, res) => {
   if (req.session.user) {
     if (req.session.user[0].userType === "admin") {
       var moderator = {
-		id: req.body.id,
+        id: req.body.id,
         name: req.body.name,
         username: req.body.username,
         email: req.body.email,
@@ -164,27 +164,175 @@ router.post("/moderators/edit", (req, res) => {
 });
 
 router.get("/moderators/delete/:id", (req, res) => {
-	if (req.session.user) {
-	  // console.log(req.session.user[0].userType);
-	  if (req.session.user[0].userType === "admin") {
-		userModel.delete(req.params.id, function(status){
-			if (status) {
-				res.redirect(
-				  "/admin/moderators?success=" +
-					encodeURIComponent("Moderator deleted!")
-				);
-			  } else {
-				res.redirect(
-				  "/admin/moderators?error=" + encodeURIComponent("SQL Error")
-				);
-			  }
-		});
-	  } else {
-		res.redirect("/");
-	  }
-	} else {
-	  res.redirect("/login");
-	}
-  });
+  if (req.session.user) {
+    // console.log(req.session.user[0].userType);
+    if (req.session.user[0].userType === "admin") {
+      userModel.delete(req.params.id, function (status) {
+        if (status) {
+          res.redirect(
+            "/admin/moderators?success=" +
+              encodeURIComponent("Moderator deleted!")
+          );
+        } else {
+          res.redirect(
+            "/admin/moderators?error=" + encodeURIComponent("SQL Error")
+          );
+        }
+      });
+    } else {
+      res.redirect("/");
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/usersupports", (req, res) => {
+  if (req.session.user) {
+    // console.log(req.session.user[0].userType);
+    if (req.session.user[0].userType === "admin") {
+      userModel.getAllByUserType("userSupport", function (results) {
+        //console.log(results);
+        res.render("admin/usersupports", {
+          modList: results,
+          moment: moment,
+          successMessage: req.query.success,
+        });
+      });
+    } else {
+      res.redirect("/");
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/userSupports/add", (req, res) => {
+  if (req.session.user) {
+    // console.log(req.session.user[0].userType);
+    if (req.session.user[0].userType === "admin") {
+      res.render("admin/userSupportAdd", { errorMessage: req.query.error });
+    } else {
+      res.redirect("/");
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.post("/userSupports/add", (req, res) => {
+  if (req.session.user) {
+    // console.log(req.session.user[0].userType);
+    if (req.session.user[0].userType === "admin") {
+      //   console.log(req.body.name + "here");
+      var userSupport = {
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        userType: "userSupport",
+      };
+
+      if (userSupport.password === userSupport.confirmPassword) {
+        userModel.insert(userSupport, function (status) {
+          if (status) {
+            res.redirect(
+              "/admin/userSupports?success=" +
+                encodeURIComponent("User Support Added!")
+            );
+          } else {
+            res.redirect(
+              "/admin/userSupports/add?error=" + encodeURIComponent("SQL Error")
+            );
+          }
+        });
+      } else {
+        res.redirect(
+          "/admin/userSupports/add?error=" +
+            encodeURIComponent("Passwords do not match")
+        );
+      }
+      console.log(userSupport);
+    } else {
+      res.redirect("/");
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/usersupports/edit/:id", (req, res) => {
+  if (req.session.user) {
+    // console.log(req.session.user[0].userType);
+    if (req.session.user[0].userType === "admin") {
+      userModel.getById(req.params.id, function (result) {
+        console.log(result);
+        res.render("admin/userSupportEdit", {
+          userSupport: result[0],
+          errorMessage: req.query.error,
+        });
+      });
+    } else {
+      res.redirect("/");
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.post("/userSupports/edit", (req, res) => {
+  if (req.session.user) {
+    if (req.session.user[0].userType === "admin") {
+      var userSupport = {
+        id: req.body.id,
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+      };
+
+      userModel.update(userSupport, function (status) {
+        if (status) {
+          res.redirect(
+            "/admin/userSupports?success=" +
+              encodeURIComponent("User Support Edited!")
+          );
+        } else {
+          res.redirect(
+            "/admin/userSupports?error=" + encodeURIComponent("SQL Error")
+          );
+        }
+      });
+    } else {
+      res.redirect("/");
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/userSupports/delete/:id", (req, res) => {
+  if (req.session.user) {
+    // console.log(req.session.user[0].userType);
+    if (req.session.user[0].userType === "admin") {
+      userModel.delete(req.params.id, function (status) {
+        if (status) {
+          res.redirect(
+            "/admin/userSupports?success=" +
+              encodeURIComponent("User Support deleted!")
+          );
+        } else {
+          res.redirect(
+            "/admin/userSupports?error=" + encodeURIComponent("SQL Error")
+          );
+        }
+      });
+    } else {
+      res.redirect("/");
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
 
 module.exports = router;
